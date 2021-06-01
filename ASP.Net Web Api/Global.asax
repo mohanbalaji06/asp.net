480dde1a96838d5c70eeedc5c2982430a3fa9e3c
mohanbalaji06 <50223328+mohanbalaji06@users.noreply.github.com>
2021-06-01 18:47:13 +0530

    Add files via upload

---
+++ ASP.Net Web Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationLogin.WebApiApplication" Language="C#" %>
---
+++ ASP.Net Web Api/MainController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+
+
+
+namespace WebApplicationLogin.Controllers
+{
+    //[RoutePrefix("api/MainController")]
+    public class MainController: ApiController
+    {
+
+        [HttpGet]
+        public IEnumerable<login> LogInDetails(){
+
+            using (studentdbEntities1 entities = new studentdbEntities1())
+            {
+                return entities.logins.ToList();
+            }
+            
+        }
+
+        [HttpGet]
+
+        public bool LoginAuth(string user, string pass) {
+
+            using (studentdbEntities1 entity = new studentdbEntities1()) {
+
+                var std = entity.logins.FirstOrDefault(e => e.username == user && e.password == pass);
+
+                if (std != null) {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
+        [HttpPost]
+        public bool Index()
+        {
+
+            //login validation goes here..
+            return true;
+        }
+
+    }
+}
