e9a9eb390e6f806f3b01662814f0497b223efcd8
mohanbalaji06 <50223328+mohanbalaji06@users.noreply.github.com>
2021-06-03 19:02:39 +0530

    Add files via upload

---
+++ Authorization with Web Api and MVC/Web Api Files/LoginApi/Controller/MianController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LoginApi.Controllers
+{
+    public class MianController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<login> LogInDetails()
+        {
+
+            using (studentdbEntities entities = new studentdbEntities())
+            {
+                return entities.logins.ToList();
+            }
+
+        }
+
+        [HttpGet]
+
+        public IHttpActionResult LoginAuth(string user, string pass)
+        {
+
+            using (studentdbEntities entity = new studentdbEntities())
+            {
+
+                var std = entity.logins.FirstOrDefault(e => e.username == user && e.password == pass);
+
+                if (std != null)
+                {
+                    return Ok(std);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+        }
+    }
+}
