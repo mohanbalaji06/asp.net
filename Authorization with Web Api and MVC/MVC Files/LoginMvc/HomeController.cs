e00ff543dd606f52acb1f6b7260e7117ff57aa5b
mohanbalaji06 <50223328+mohanbalaji06@users.noreply.github.com>
2021-06-03 19:12:32 +0530

    Add files via upload

---
+++ Authorization with Web Api and MVC/MVC Files/LoginMvc/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginMVC.Models;
+using System.Net.Http;
+
+
+namespace LoginMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LoginModel model)
+        {
+           
+
+            using(var client=new HttpClient())
+            {
+                string url = "http://localhost/api/Mian?user="+model.username+"&pass="+model.password;
+                
+                var response =client.GetAsync(url);
+
+                response.Wait();
+
+
+                var result = response.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    return View();
+                }
+                else {
+                    return View("LoginFail");
+                }
+            }
+        }
+	}
+}
---
+++ Authorization with Web Api and MVC/MVC Files/LoginMvc/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginMVC.Models
+{
+    public class LoginModel
+    {
+
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
